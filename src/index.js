4c64c366670dbd26b59d2bea9938c1a47730fccf
Taras-Novitskyi <novitskyitaras@gmail.com>
2023-02-02 23:48:30 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,9 +11,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       {/* <PersistGate loading={null} persistor={persistor}> */}
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
+      <BrowserRouter basename="/practic-with-mentor-6">
+        <App />
+      </BrowserRouter>
       {/* </PersistGate> */}
     </Provider>
   </React.StrictMode>
